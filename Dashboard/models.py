369fc348ea336133934a9017f0da64214ef75873
Toygoon <toygoon@gmail.com>
2022-12-06 00:48:52 +0900

    Add: CPU Usage Model, Client Info Model

--- Dashboard/models.py
+++ Dashboard/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+ 
 # Create your models here.
+class UsageCPU(models.Model):
+    usage = models.FloatField()
+    time = models.DateTimeField(auto_now_add=True)
+
+
+class ClientInfo(models.Model):
+    internal_ip = models.CharField(max_length=50)
+    external_ip = models.CharField(max_length=50)
