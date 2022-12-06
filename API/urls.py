05525013ce2126cc979d13b346e899c5e56d8386
Toygoon <toygoon@gmail.com>
2022-12-06 11:32:05 +0900

    Add: Connect with Client Info API

--- API/urls.py
+++ API/urls.py
@@ -5,4 +5,5 @@ from API import views
 urlpatterns = [
     path('hello/', views.HelloRequest.as_view(), name='hello_request'),
     path('cpu_usage/', views.CPUUsage.as_view(), name='cpu_usage'),
+    path('client_info/', views.ClientInfo.as_view(), name='client_info'),
 ]
