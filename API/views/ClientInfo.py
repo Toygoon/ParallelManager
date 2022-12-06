2102f80cb901adbc2914570af86b19f8fef7f529
Toygoon <toygoon@gmail.com>
2022-12-06 11:32:47 +0900

    Add: Add cards for client info in dashboard

--- API/views/ClientInfo.py
+++ API/views/ClientInfo.py
@@ -1,9 +1,15 @@
+import os
+import platform
 import socket
 import subprocess
 
+import psutil
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.views import APIView
 
 import Dashboard.models
+from API.serializer import ClientInfoSerializer
 
 
 class ClientInfo(APIView):
@@ -18,4 +24,17 @@ class ClientInfo(APIView):
         if info is None:
             info = Dashboard.models.ClientInfo()
             info.internal_ip = socket.gethostbyname(socket.gethostname())
-            info.external_ip = subprocess.check_output(['curl', 'ifconfig.co'])
+            e = str(subprocess.check_output(['curl', 'ifconfig.co']).strip())
+            info.external_ip = e[2:-1]
+
+            info.os = platform.system()
+            info.version = platform.version()
+            info.processor = platform.processor()
+            info.arch = platform.machine()
+            info.total_ram = str(round(psutil.virtual_memory().total / (1024.0 ** 3))) + " GB"
+
+            info.save()
+
+        serializer = ClientInfoSerializer(info)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
